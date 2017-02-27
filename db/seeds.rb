f6aff670cf20e46902b8e197da91cee2d2db1c49
Simon Cheng <scheng365@gmail.com>
2017-02-26 19:36:59 -0500

    Added database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+article = Article.create([
+    {title: "Stratups 101", text: "A tale of a boxer."},
+    {title: "How to make cereal", text: "The return of the Jedi."},
+    {title: "5 ways to increase energy", text: "A young wizard finds himself."},
+    {title: "Is halloween dead?", text: "Best movie ever."},
+    {title: "Getting ready for winter", text: "A lost fish."},
+    {title: "Redbull in Ottawa", text: "Is the mission really possible?"},
+  ])
